70cb48a4f53737fad724390ad9e52b99c8b85438
trooster <trooster@interstroom.nl>
2009-12-30 21:01:23 +0100

    with slash...

--- generators/easy_swf_upload/templates/jsProgressBarHandler.js
+++ generators/easy_swf_upload/templates/jsProgressBarHandler.js
@@ -43,8 +43,8 @@
 			animate		: true,										// Animate the progress? - default: true
 			showText	: true,										// show text with percentage in next to the progressbar? - default : true
 			width		: 120,										// Width of the progressbar - don't forget to adjust your image too!!!
-			boxImage	: 'images/percentImage.png',			// boxImage : image around the progress bar
-			barImage	: 'images/percentImage_back.png',	// Image to use in the progressbar. Can be an array of images too.
+			boxImage	: '/images/percentImage.png',			// boxImage : image around the progress bar
+			barImage	: '/images/percentImage_back.png',	// Image to use in the progressbar. Can be an array of images too.
 			height		: 12,										// Height of the progressbar - don't forget to adjust your image too!!!
 			onTick		: function(pbObj) { return true }
 		}
